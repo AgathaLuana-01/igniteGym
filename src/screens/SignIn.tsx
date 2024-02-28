cd6ca0fe202877c54ecb289c6a0d85651ef43ee5
AgathaLuana-01 <agathaluana.ads@gmail.com>
2024-02-28 14:22:02 -0300

    Finalizando a SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center, Heading } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
@@ -8,42 +8,52 @@ import { Button } from '@components/Button';
 
 export function SingnIn() {
     return (
-        <VStack flex={1} bg={'gray.700'} px={10}>
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsHorizontalScrollIndicator={false}>
+            <VStack flex={1} bg={'gray.700'} px={10} pb={16}>
 
-            <Image
-                source={BackgroundImg}
-                alt='Pessoas treinando'
-                resizeMode='contain'
-                position={'absolute'}
-            />
-
-            <Center my={24}>
-                <LogoSvg />
-                <Text color='gray.100' fontSize='sm'>
-                    Treine sua mente e o seu corpo
-                </Text>
-            </Center>
-
-            <Center>
-                <Heading color='gray.100' fontSize={'xl'} mb={6} fontFamily={'heading'}>
-                    Acesse sua conta
-                </Heading>
-
-                <Input
-                    placeholder='E-mail'
-                    keyboardType='email-address'
-                    autoCapitalize='none'
-                />
-                <Input
-                    placeholder='Senha'
-                    secureTextEntry={true}
+                <Image
+                    source={BackgroundImg}
+                    alt='Pessoas treinando'
+                    resizeMode='contain'
+                    position={'absolute'}
                 />
 
-                <Button title='Acessar' />
-            </Center>
+                <Center my={24}>
+                    <LogoSvg />
+                    <Text color='gray.100' fontSize='sm'>
+                        Treine sua mente e o seu corpo
+                    </Text>
+                </Center>
 
-            <Button title='Criar Conta' variant={'outline'} />
-        </VStack>
+                <Center>
+                    <Heading color='gray.100' fontSize={'xl'} mb={6} fontFamily={'heading'}>
+                        Acesse sua conta
+                    </Heading>
+
+                    <Input
+                        placeholder='E-mail'
+                        keyboardType='email-address'
+                        autoCapitalize='none'
+                    />
+                    <Input
+                        placeholder='Senha'
+                        secureTextEntry={true}
+                    />
+
+                    <Button title='Acessar' />
+                </Center>
+
+                <Center mt={24}>
+                    <Text color={'gray.100'} fontSize={'sm'} mb={3} fontFamily={'body'}>
+                        Ainda não tem acesso?
+                    </Text>
+
+                    <Button
+                        title='Criar Conta'
+                        variant={'outline'} />
+                </Center>
+            </VStack>
+        </ScrollView>
     );
 
 }
