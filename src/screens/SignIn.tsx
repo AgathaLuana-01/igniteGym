6cb8fe07169423bf734861778500d002006cf1a2
Ágatha Luana <agathaluana@escavador.com>
2024-03-01 14:18:06 -0300

    modificando o tema de cor padrão do react

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,59 +1,60 @@
-import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
-import LogoSvg from '@assets/logo.svg'
-import BackgroundImg from '@assets/background.png'
+import LogoSvg from "@assets/logo.svg";
+import BackgroundImg from "@assets/background.png";
 
-import { Input } from '@components/Input';
-import { Button } from '@components/Button';
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function SignIn() {
-    return (
-        <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsHorizontalScrollIndicator={false}>
-            <VStack flex={1} bg={'gray.700'} px={10} pb={16}>
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsHorizontalScrollIndicator={false}
+    >
+      <VStack flex={1} px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          alt="Pessoas treinando"
+          resizeMode="contain"
+          position={"absolute"}
+        />
 
-                <Image
-                    source={BackgroundImg}
-                    alt='Pessoas treinando'
-                    resizeMode='contain'
-                    position={'absolute'}
-                />
+        <Center my={24}>
+          <LogoSvg />
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
 
-                <Center my={24}>
-                    <LogoSvg />
-                    <Text color='gray.100' fontSize='sm'>
-                        Treine sua mente e o seu corpo
-                    </Text>
-                </Center>
+        <Center>
+          <Heading
+            color="gray.100"
+            fontSize={"xl"}
+            mb={6}
+            fontFamily={"heading"}
+          >
+            Acesse sua conta
+          </Heading>
 
-                <Center>
-                    <Heading color='gray.100' fontSize={'xl'} mb={6} fontFamily={'heading'}>
-                        Acesse sua conta
-                    </Heading>
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry={true} />
 
-                    <Input
-                        placeholder='E-mail'
-                        keyboardType='email-address'
-                        autoCapitalize='none'
-                    />
-                    <Input
-                        placeholder='Senha'
-                        secureTextEntry={true}
-                    />
+          <Button title="Acessar" />
+        </Center>
 
-                    <Button title='Acessar' />
-                </Center>
+        <Center mt={24}>
+          <Text color={"gray.100"} fontSize={"sm"} mb={3} fontFamily={"body"}>
+            Ainda não tem acesso?
+          </Text>
 
-                <Center mt={24}>
-                    <Text color={'gray.100'} fontSize={'sm'} mb={3} fontFamily={'body'}>
-                        Ainda não tem acesso?
-                    </Text>
-
-                    <Button
-                        title='Criar Conta'
-                        variant={'outline'} />
-                </Center>
-            </VStack>
-        </ScrollView>
-    );
-
+          <Button title="Criar Conta" variant={"outline"} />
+        </Center>
+      </VStack>
+    </ScrollView>
+  );
 }
