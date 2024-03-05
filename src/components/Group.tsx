83c0ed4befa53d9c14f81f1c01da700302874d89
Ágatha Luana <agathaluana@escavador.com>
2024-03-05 15:00:54 -0300

    Estilizando evento de Pressed

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,23 +1,26 @@
-import { Pressable, Text , IPressableProps} from "native-base";
+import { Pressable, Text, IPressableProps } from "native-base";
 
-type Props = IPressableProps &{
+type Props = IPressableProps & {
   name: string;
 };
 
 export function Group({ name, ...rest }: Props) {
   return (
     <Pressable
-    mr={3}
-    w={24}
-    h={10}
-    bg={'gray.600'}
-    rounded={'md'}
-    justifyContent={'center'}
-    alignItems={'center'}
-    overflow={'hidden'}
-    {...rest}
+      mr={3}
+      w={24}
+      h={10}
+      bg={"gray.600"}
+      rounded={"md"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      overflow={"hidden"}
+      _pressed={{
+        borderColor: "green.500",
+        borderWidth: 1,
+      }}
+      {...rest}
     >
-
       <Text
         color={"gray.200"}
         textTransform={"uppercase"}
