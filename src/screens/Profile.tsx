7dd3bc8546b30ac3d3f541d70d1b61d6b7c2f929
Ágatha Luana <agathaluana@escavador.com>
2024-03-06 11:47:18 -0300

    Inputs de senha

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,18 @@
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { ScrollView, Center, VStack, Text, Skeleton } from "native-base";
+import {
+  ScrollView,
+  Center,
+  VStack,
+  Text,
+  Skeleton,
+  Heading,
+} from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 const PHOTO_SIZE = 33;
 
@@ -49,9 +57,19 @@ export function Profile() {
             isDisabled
           />
         </Center>
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color={"gray.200"} fontSize={"md"} mb={2}>
+            Alterar senha
+          </Heading>
 
+          <Input bg={"gray.600"} placeholder="Senha antiga" secureTextEntry />
+          <Input bg={"gray.600"} placeholder="Nova senha" secureTextEntry />
+          <Input bg={"gray.600"} placeholder="Confirme a nova senha" secureTextEntry />
 
-        
+          <Button title="Atualizar"
+          mt={4}
+          />
+        </VStack>
       </ScrollView>
     </VStack>
   );
