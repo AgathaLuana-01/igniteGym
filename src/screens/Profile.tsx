8f2e6d47c0bc047f12708e781b0b4302b641932c
Ágatha Luana <agathaluana@escavador.com>
2024-03-06 17:58:59 -0300

    Recuperando foto selecionada

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -21,12 +21,17 @@ export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
 
   async function handleUserPhotoSelect() {
-    await ImagePicker.launchImageLibraryAsync({
+   const photoSelected = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
       quality: 1,
       aspect: [4, 4],
       allowsEditing: true,
+      base64: true
     });
+    console.log(photoSelected);
+    if(photoSelected.canceled){
+      return;
+    }
   }
 
   return (
