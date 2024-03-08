df1beb63cb2c62148f859812fb7b3ddc55fd9b33
Ágatha Luana <agathaluana@escavador.com>
2024-03-08 15:21:37 -0300

    Axios Exceptions

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -5,12 +5,14 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 import { api } from "@services/api";
+import axios from "axios";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { Alert } from "react-native";
 
 type FormDataProps = {
   name: string;
@@ -46,7 +48,14 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, email, password }: FormDataProps) {
-    const response = await api.post("/users", {name, email, password});
+    try {
+      const response = await api.post("/users", { name, email, password });
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        Alert.alert(error.response?.data.message);
+      }
+    }
   }
 
   return (
