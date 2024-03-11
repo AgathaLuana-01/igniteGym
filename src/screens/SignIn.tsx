4dcb8158e954a0210472359b6385a55d25a390d3
Ágatha Luana <agathaluana@escavador.com>
2024-03-11 13:31:05 -0300

    Feedback de SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  useToast,
+} from "native-base";
 
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 
@@ -11,6 +20,7 @@ import BackgroundImg from "@assets/background.png";
 
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { AppError } from "@utils/AppError";
 
 type FormData = {
   email: string;
@@ -18,9 +28,10 @@ type FormData = {
 };
 
 export function SignIn() {
+  const [ isLoading, setIsLoading] = useState(false);
   const { signIn } = useAuth();
-
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const toast = useToast();
 
   const {
     control,
@@ -32,8 +43,24 @@ export function SignIn() {
     navigation.navigate("signUp");
   }
 
- async function handleSignIn({ email, password }: FormData) {
-   await signIn(email, password);
+  async function handleSignIn({ email, password }: FormData) {
+    try {
+      setIsLoading(true);
+      await signIn(email, password);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError
+        ? error.message
+        : "Não foi possível entrar. Tente novamente mais tarde.";
+        setIsLoading(false);
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+     
+    }
   }
 
   return (
@@ -95,7 +122,7 @@ export function SignIn() {
             )}
           />
 
-          <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
+          <Button title="Acessar" onPress={handleSubmit(handleSignIn)} isLoading={isLoading}/>
         </Center>
 
         <Center mt={10}>
