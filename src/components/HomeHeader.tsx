25e694c4e260ac30ed6045e82df225571d8c1c65
Ágatha Luana <agathaluana@escavador.com>
2024-03-11 14:38:16 -0300

    Exibindo o nome do usuario

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,14 +1,20 @@
 import { HStack, Heading, Icon, Text, VStack } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 
+import { useAuth } from "@hooks/useAuth";
 import { UserPhoto } from "./UserPhoto";
+
 import { TouchableOpacity } from "react-native";
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems={"center"}>
       <UserPhoto
-        source={{ uri: "https://i.pinimg.com/originals/69/74/4f/69744fdda9a70b32e4f2f3020b687746.jpg" }}
+        source={{
+          uri: "https://i.pinimg.com/originals/69/74/4f/69744fdda9a70b32e4f2f3020b687746.jpg",
+        }}
         alt="imagem do usuária"
         size={16}
         mr={4}
@@ -18,7 +24,7 @@ export function HomeHeader() {
           Olá,
         </Text>
         <Heading color="gray.100" fontSize={"md"}>
-          Ágatha
+          {user.name}
         </Heading>
       </VStack>
       <TouchableOpacity>
