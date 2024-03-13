fd7dd4323df8397ab995b4b9b1ad02c65b464834
Ágatha Luana <agathaluana@escavador.com>
2024-03-13 12:54:52 -0300

    Exibindo dados do histórico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -7,18 +7,12 @@ import { HistoryCard } from "@components/HistoryCard";
 import { AppError } from "@utils/AppError";
 import { api } from "@services/api";
 import { useFocusEffect } from "@react-navigation/native";
+import { HistoryByDayDTO } from "@dtos/HistoryByDayDTO";
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true);
-  const [exercises, setExercises] = useState([
-    {
-      title: "26.08.22",
-      data: ["Puxada frontal", "Remada unilateral"],
-    },
-    {
-      title: "27.08.22",
-      data: ["Agachamento"],
-    },
+  const [exercises, setExercises] = useState<HistoryByDayDTO[]>([
+    
   ]);
   const toast = useToast();
 
@@ -26,7 +20,7 @@ export function History() {
     try {
       setIsLoading(true);
       const response = await api.get("/history");
-      console.log(response.data);
+      setExercises(response.data);
     } catch (error) {
       const isAppError = error instanceof AppError;
       const title = isAppError
@@ -51,8 +45,8 @@ export function History() {
       <ScreenHeader title="Histórico de Exercícios" />
       <SectionList
         sections={exercises}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => <HistoryCard />}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <HistoryCard data={item}/>}
         renderSectionHeader={({ section }) => (
           <Heading color={"gray.200"} fontSize={"md"} mt={10} mb={3}>
             {section.title}
