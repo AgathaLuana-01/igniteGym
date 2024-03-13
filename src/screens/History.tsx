f86ca437489610b17ea059fd23c82f48b61c0a78
Ágatha Luana <agathaluana@escavador.com>
2024-03-13 12:24:04 -0300

    Carregando historico

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
-import { VStack, SectionList, Heading, Text } from "native-base";
+import { useCallback, useState } from "react";
+import { VStack, SectionList, Heading, Text, useToast } from "native-base";
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { HistoryCard } from "@components/HistoryCard";
 
+import { AppError } from "@utils/AppError";
+import { api } from "@services/api";
+import { useFocusEffect } from "@react-navigation/native";
+
 export function History() {
+  const [isLoading, setIsLoading] = useState(true);
   const [exercises, setExercises] = useState([
     {
       title: "26.08.22",
@@ -15,6 +20,31 @@ export function History() {
       data: ["Agachamento"],
     },
   ]);
+  const toast = useToast();
+
+  async function fetchHistory() {
+    try {
+      setIsLoading(true);
+      const response = await api.get("/history");
+      console.log(response.data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível carregar o histórico. ";
+
+      toast.show({ title, placement: "top", bgColor: "red.500" });
+    } finally{
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchHistory();
+    }, [])
+  );
+
 
   return (
     <VStack flex={1}>
@@ -29,10 +59,12 @@ export function History() {
           </Heading>
         )}
         px={8}
-        contentContainerStyle={exercises.length === 0 && {flex: 1 , justifyContent: 'center'}}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: "center" }
+        }
         ListEmptyComponent={() => (
-          <Text color={'gray.100'} textAlign={'center'}>
-            Não há exercícios registrados ainda. {'\n'}
+          <Text color={"gray.100"} textAlign={"center"}>
+            Não há exercícios registrados ainda. {"\n"}
             Vamos fazer exercícios hoje?
           </Text>
         )}
