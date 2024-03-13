7ecd3339358607fdb48311f52193cd6809012e3f
Ágatha Luana <agathaluana@escavador.com>
2024-03-13 17:38:27 -0300

    Atualizando dados do usuário no starage e estado

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -64,7 +64,7 @@ export function Profile() {
   );
 
   const toast = useToast();
-  const { user } = useAuth();
+  const { user, updateUserProfile } = useAuth();
   const {
     control,
     handleSubmit,
@@ -112,7 +112,11 @@ export function Profile() {
   async function handleProfileUpdate(data: FormDataProps) {
     try {
       setIsUpdating(true);
+
+      const userUpdated = user;
+      userUpdated.name = data.name;
       await api.put("/users", data);
+      await updateUserProfile(userUpdated);
       toast.show({
         title: "Perfil atualizado com sucesso!",
         placement: "top",
