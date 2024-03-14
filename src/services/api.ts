68b78772d88ee1ae26b8df18105d3cfda0f4c848
Ágatha Luana <agathaluana@escavador.com>
2024-03-14 13:52:31 -0300

    Incluindo gerenciador de Token na API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,19 +1,28 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 import { AppError } from "@utils/AppError";
+
+type SignOut = () => void;
+
+type APIInstaceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+};
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:3333",
-});
+}) as APIInstaceProps;
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.data) {
-      return Promise.reject(new AppError(error.response.data.message));
-    } else {
-      return Promise.reject(error);
+api.registerInterceptTokenManager = (signOut) => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response && error.response.data) {
+        return Promise.reject(new AppError(error.response.data.message));
+      } else {
+        return Promise.reject(error);
+      }
     }
-  }
-);
+  );
+  return () => api.interceptors.response.eject(interceptTokenManager);
+};
 
 export { api };
