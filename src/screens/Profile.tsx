06d58f34cea9a0efc5e9e6929f406e949007d899
Ágatha Luana <agathaluana@escavador.com>
2024-03-14 12:02:30 -0300

    fazendo upload da imagem

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -105,9 +105,22 @@ export function Profile() {
         const photoFile = {
           name: `${user.name}.${fileExtension}`.toLowerCase(),
           uri: photoSelected.assets[0].uri,
-          type:`${photoSelected.assets[0].type}/${fileExtension}`
-        };
-        console.log(photoFile);
+          type: `${photoSelected.assets[0].type}/${fileExtension}`,
+        } as any;
+
+        const userPhotoUploadForm = new FormData();
+        userPhotoUploadForm.append("avatar", photoFile);
+        await api.patch("/users/avatar", userPhotoUploadForm, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+
+        toast.show({
+          title: "Foto atualizada!",
+          placement: "top",
+          bgColor: "green.500",
+        });
       }
     } catch (error) {
       console.log(error);
