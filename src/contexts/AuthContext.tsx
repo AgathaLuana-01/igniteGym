6d51ba864f444058d9b63f85c8588aa800a3495f
Ágatha Luana <agathaluana@escavador.com>
2024-03-15 10:44:59 -0300

    Armazenando o Refresh Token

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -40,12 +40,12 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     setUser(userData);
   }
 
-  async function storagUserAndTokeneSave(userData: UserDTO, token: string) {
+  async function storagUserAndTokeneSave(userData: UserDTO, token: string, refresh_token: string) {
     try {
       setIsLoadingUserStorageData(true);
 
       await storageUserSave(userData);
-      await storageAuthTokenSave(token);
+      await storageAuthTokenSave({token, refresh_token});
     } catch (error) {
       throw error;
     } finally {
@@ -57,8 +57,8 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     try {
       const { data } = await api.post("/sessions", { email, password });
 
-      if (data.user && data.token) {
-        await storagUserAndTokeneSave(data.user, data.token);
+      if (data.user && data.token && data.refresh_token) {
+        await storagUserAndTokeneSave(data.user, data.token, data.refresh_token);
         userAndTokenUpdate(data.user, data.token);
       }
     } catch (error) {
@@ -95,7 +95,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
       setIsLoadingUserStorageData(true);
 
       const userLogged = await storageUserGet();
-      const token = await storageAuthTokenGet();
+      const {token} = await storageAuthTokenGet();
       if (token && userLogged) {
         userAndTokenUpdate(userLogged, token);
       }
